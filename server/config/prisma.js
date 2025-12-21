6041672773feb14fd54b1a62252cd9969b8839c8
JUCILY117 <111986929+JUCILY117@users.noreply.github.com>
2025-12-21 13:18:24 +0530

    refactor: update package.json dependencies and scripts, remove prisma.config.ts, and add .gitignore
    - Changed the "dev" script to run both nodemon and prisma studio.
    - Updated @prisma/client and prisma versions in dependencies and devDependencies.
    - Removed prisma.config.ts file and migrated its configuration to prisma.config.js.
    - Updated schema.prisma to use environment variable for DATABASE_URL.
    - Added .gitignore file to exclude unnecessary files and directories.

--- server/config/prisma.js
+++ server/config/prisma.js
@@ -1,17 +1,6 @@
-import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 import 'dotenv/config';
 
-const connectionString = process.env.DATABASE_URL;
-
-if (!connectionString) {
-    throw new Error('DATABASE_URL is not defined');
-}
-
-const adapter = new PrismaPg({ connectionString });
-
-const prisma = new PrismaClient({
-    adapter,
-});
+const prisma = new PrismaClient();
 
 export default prisma;
